d35e3dd3ccac67229f80073b1b7a834abc7278b3
Nick Pavlosky <npavlosky@gmail.com>
2024-11-09 21:00:32 -0800

    Basic notes app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useSearchParams } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [content, setContent] = useState(searchParams.get("content") || "");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <h1>Pigeon Note</h1>
+      <textarea
+        autoFocus
+        value={content}
+        onChange={(e) => {
+          if (e.target.value) {
+            setSearchParams({ content: e.target.value });
+          } else {
+            setSearchParams({});
+          }
+          setContent(e.target.value);
+        }}
+        placeholder="Write something down..."
+      />
+      <p className="attribution">
+        <a
+          href="https://www.flaticon.com/free-icons/pigeon"
+          title="pigeon icons"
+        >
+          Pigeon icon created by Freepik - Flaticon
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
       </p>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const basename = process.env.NODE_ENV === "production" ? "/pigeon-note" : "/";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+  ],
+  { basename },
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
